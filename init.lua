925842e6d440eda307ec1893d2f6e91bb6db6c17
Ian <work@quanianitis.com>
2024-10-14 02:08:45 +0800

    update: themes and stuff

--- init.lua
+++ init.lua
@@ -829,16 +829,23 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    'nvimdev/pure.vim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'pure'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
+      vim.cmd.hi 'Normal ctermbg=none'
+      vim.cmd [[
+        hi Normal guibg=NONE ctermbg=NONE
+        hi LineNr guibg=NONE
+        hi SignColumn guibg=NONE
+        hi EndOfBuffer guibg=NONE
+      ]]
     end,
   },
 
